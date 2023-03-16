3ad82aa92a48a2d9ee3959ac12acd22e968e5892
Philip Nwachukwu <philip.i.nwachukwu@gmail.com>
2023-03-16 17:24:14 +0000

    Update kubectl

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -1,8 +1,8 @@
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
   version         = "~> 19.0"
-  cluster_name    = "myapp-eks-cluster"
-  cluster_version = "1.25"
+  cluster_name    = "myaltschoolapp-eks-cluster"
+  cluster_version = "1.24"
 
   cluster_endpoint_public_access = true
 
@@ -11,13 +11,13 @@ module "eks" {
 
   tags = {
     environment = "development"
-    application = "myapp"
+    application = "myaltschoolapp"
   }
 
   eks_managed_node_groups = {
     dev = {
       min_size     = 2
-      max_size     = 3
+      max_size     = 4
       desired_size = 2
 
       instance_types = ["t2.medium"]
